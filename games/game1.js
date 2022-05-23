9da7ae7117f4af49c5ec3f801193ebf9de60cbfc
TusharKumar15 <84404999+TusharKumar15@users.noreply.github.com>
2022-05-23 16:40:28 +0530

    Update game1.js

--- games/game1.js
+++ games/game1.js
@@ -1,6 +1,3 @@
-function tictac(){
-    // document.getElementById("game-board").innerHTML = "welcome to game 1";
-    console.log("tictac accesible");
+export function tictac(){
+    document.getElementById("game-board").innerHTML = "welcome to game 1: tictac";
 }
-
-export {tictac};
